872d6468a83c8f3be9b2305a16907905c52002fc
Omar <omaar_5@hotmail.com>
2021-07-24 23:22:06 +0200

    fix: start Project

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <link rel="icon" href="/favicon.png" />
+        <title>E-commerce Web-Site</title>
+      </Head>
+
+      <h1 className="text-5xl">Let's build an E-commerce website!</h1>
+    </div>
+  );
+}
