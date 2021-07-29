0a2bdda2380b50c3a8dc7a17a5ef04acb3470160
Omar <omaar_5@hotmail.com>
2021-07-29 19:47:04 +0200

    feat: setup firebase

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAIWK4F6goKH8ODnHH8RN8aNvKeTuBaF3Y",
+    authDomain: "clone-ddb5e.firebaseapp.com",
+    projectId: "clone-ddb5e",
+    storageBucket: "clone-ddb5e.appspot.com",
+    messagingSenderId: "82171991693",
+    appId: "1:82171991693:web:49f0046f6232e067feb4c4"
+};
