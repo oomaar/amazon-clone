18064df4c6d91907b6c95900dfbbfab793a15b06
Omar <omaar_5@hotmail.com>
2021-07-31 01:12:25 +0200

    select: items & update num. of items

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -8,11 +8,14 @@ import {
     ShoppingCartIcon,
     MenuIcon
 } from "@heroicons/react/outline";
+import { useSelector } from "react-redux";
+import { selectItems } from "../../slices/basketSlice";
 
 export const Header = () => {
     const [sidebarShow, setSidebarShow] = useState(false);
     const [session] = useSession();
     const router = useRouter();
+    const items = useSelector(selectItems);
 
     return (
         <div>
@@ -62,7 +65,7 @@ export const Header = () => {
                         >
                             <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400
                             text-center rounded-full text-black font-bold">
-                                0
+                                {items.length}
                             </span>
                             <ShoppingCartIcon className="h-10" />
                             <p className="hidden md:inline font-extrabold md:text-sm mt-2">Basket</p>
