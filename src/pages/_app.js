8778fe9b321555ab3fc5289aff74df6c4b799562
Omar <omaar_5@hotmail.com>
2021-07-30 22:36:42 +0200

    refactor: Global for all pages (Header & Footer)

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,12 +2,16 @@ import { Provider } from 'react-redux';
 import { store } from '../app/store';
 import '../styles/globals.css';
 import { Provider as AuthProvider } from "next-auth/client";
+import { Footer, Header } from '../Components';
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <AuthProvider session={pageProps.session}>
       <Provider store={store}>
+        <Header />
+        <div className="h-24" />
         <Component {...pageProps} />
+        <Footer />
       </Provider>
     </AuthProvider>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,13 +9,10 @@ export default function Home({ products }) {
         <title>E-commerce Web-Site</title>
       </Head>
 
-      <Header />
-
       <main className="max-w-screen-2xl mx-auto">
         <Banner />
         <ProductFeed products={products} />
       </main>
-      <Footer />
     </div>
   );
 };
