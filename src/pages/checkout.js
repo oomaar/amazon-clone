22450d375e322c72aee4a3c3762c2d19cded95f7
Omar <omaar_5@hotmail.com>
2021-07-30 22:36:50 +0200

    feat: setup Checkout Page

---
+++ src/pages/checkout.js
@@ -0,0 +1,11 @@
+import { Header } from "../Components";
+
+const Checkout = () => {
+    return (
+        <div className="bg-gray-700">
+            <h1>Checkout Page</h1>
+        </div>
+    );
+};
+
+export default Checkout;
