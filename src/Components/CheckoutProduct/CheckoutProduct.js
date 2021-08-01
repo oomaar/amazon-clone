90ec30b3a7fd33f0ae9ab547c5075b8315c62893
Omar <omaar_5@hotmail.com>
2021-08-01 02:46:50 +0200

    feat: CheckoutProduct part-2

--- src/Components/CheckoutProduct/CheckoutProduct.js
+++ src/Components/CheckoutProduct/CheckoutProduct.js
@@ -1,8 +1,40 @@
 import { StarIcon } from "@heroicons/react/solid";
 import Image from "next/image";
 import Currency from "react-currency-formatter";
+import { useDispatch } from "react-redux";
+import { addToBasket, removeFromBasket } from "../../slices/basketSlice";
 
 export const CheckoutProduct = ({ item }) => {
+    const dispatsh = useDispatch(addToBasket);
+
+    const id = item.id;
+    const title = item.title;
+    const price = item.price;
+    const description = item.description;
+    const category = item.category;
+    const image = item.image;
+    const rating = item.rating;
+    const hasPrime = item.hasPrime;
+
+    const addItemToBasket = () => {
+        const product = {
+            id,
+            title,
+            price,
+            description,
+            category,
+            image,
+            rating,
+            hasPrime
+        };
+
+        dispatsh(addToBasket(product));
+    };
+
+    const removeItemFromBasket = () => {
+        dispatsh(removeFromBasket({ id }));
+    };
+
     return (
         <div className="grid grid-cols-5 bg-gray-700 p-2 rounded-md">
             <div className="flex justify-center shadow-md transform hover:scale-105 cursor-pointer
@@ -17,15 +49,19 @@ export const CheckoutProduct = ({ item }) => {
 
             <div className="col-span-3 mx-5">
                 <p>{item.title}</p>
+
                 <div className="flex">
                     {Array(item.rating).fill().map((_, i) => (
                         <StarIcon key={i} className="h-5 text-yellow-500" />
                     ))}
                 </div>
+
                 <p className="text-xs my-2 line-clamp-3">{item.description}</p>
+
                 <div className="mb-5 text-gray-100">
                     <Currency quantity={item.price} currency='EGP' />
                 </div>
+
                 {item.hasPrime && (
                     <div className="flex items-center space-x-2 my-5">
                         <img
@@ -40,8 +76,18 @@ export const CheckoutProduct = ({ item }) => {
             </div>
 
             <div className="flex flex-col justify-self-end space-y-2 my-auto">
-                <button className="button mt-auto text-gray-900">Add to Basket</button>
-                <button className="button mt-auto text-gray-900">Remove from Basket</button>
+                <button
+                    className="button mt-auto text-gray-900"
+                    onClick={addItemToBasket}
+                >
+                    Add to Basket
+                </button>
+                <button
+                    className="button mt-auto text-gray-900"
+                    onClick={removeItemFromBasket}
+                >
+                    Remove from Basket
+                </button>
             </div>
         </div>
     );
