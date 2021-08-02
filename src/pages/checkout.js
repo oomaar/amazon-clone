2feca54a263b5276faafc37e2216e36e28f1ee47
Omar <omaar_5@hotmail.com>
2021-08-02 20:51:50 +0200

    feat: Checkout page part-2

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,12 +1,17 @@
 import Image from "next/image";
 import Head from "next/head";
 import { useSelector } from "react-redux";
-import { selectItems } from "../slices/basketSlice";
+import { selectItems, selectTotal } from "../slices/basketSlice";
 import { ShoppingCartIcon } from "@heroicons/react/outline";
 import { CheckoutProduct } from "../Components";
+import Currency from "react-currency-formatter";
+import { useSession } from "next-auth/client";
 
 const Checkout = () => {
     const items = useSelector(selectItems);
+    const total = useSelector(selectTotal);
+    const [session] = useSession();
+
     return (
         <div className="bg-gray-700">
             <Head>
@@ -16,7 +21,6 @@ const Checkout = () => {
 
             {/* lg:flex makes it not responsive in small and medium screens */}
             <main className="lg:flex max-w-screen-2xl mx-auto">
-                {/* Left */}
                 <div className="flex-grow m-5 shadow-sm">
                     <Image
                         src="https://links.papareact.com/ikj"
@@ -50,8 +54,30 @@ const Checkout = () => {
                     </div>
                 </div>
 
-                {/* Right */}
-                <div></div>
+                <div className="flex flex-col shadow-md bg-gray-800 p-10 lg:rounded-md lg:mt-4 lg:mr-4">
+                    {items.length > 0 && (
+                        <>
+                            <h2 className="whitespace-nowrap text-gray-100">
+                                Subtotal ({items.length})
+                                {items.length > 1 ? " Items" : " Item"}: {" "}
+                                <span className="font-bold text-gray-400">
+                                    <Currency quantity={total} currency='EGP' />
+                                </span>
+                            </h2>
+                            <button
+                                disabled={!session}
+                                className={
+                                    `
+                                button mt-2 
+                                ${!session && "from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed"}
+                                `
+                                }
+                            >
+                                {!session ? "Sign in to checkout" : "Proceed to checkout"}
+                            </button>
+                        </>
+                    )}
+                </div>
             </main>
         </div>
     );
